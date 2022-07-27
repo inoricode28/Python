762d699dbd6fe8badce658aba7f2438bc56f26a0
inoricode28 <inori237@gmail.com>
2022-07-27 10:55:00 -0500

    actualizacion_ejer02

--- Idat_Python/Semana08/ejer02.py
+++ Idat_Python/Semana08/ejer02.py
@@ -2,10 +2,10 @@
 #la siguiente serie: 3, 10, 17, ..., 94
 i=3
 suma=0
-contador=0
+
 while i<=94:
     print(i,end=" ")
     suma+=i #Acumulador
     i+=7
-    contador+=1 #Contador
+    
 print("\suma: "+str(suma))

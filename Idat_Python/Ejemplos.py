136ce0a596ed85fa840047f1202d326a96e09e04
inoricode28 <inori237@gmail.com>
2022-08-07 10:58:05 -0500

    Primer_Ciclo

--- Idat_Python/Ejemplos.py
+++
@@ -1,8 +0,0 @@
-#Ejemplo de funcion print con un parametro
-#
-print("Hola mundo")
-print("Hola mundo","bienvenido a IDAT")
-print("Hola mundo\nbienvenidos a IDAT")
-
-print("Peru campeon", "rumbo a Qatar","2022")
-print("Vamos ","Peru campeon", "rumbo a Qatar","2022",sep="-")

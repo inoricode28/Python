11e29c40b82036c52e9b1dfbda36306be9c77427
inoricode28 <inori237@gmail.com>
2022-08-07 10:56:27 -0500

    ejemplos

---
+++ Idat_Python/Ejemplos_01.py
@@ -0,0 +1,11 @@
+def saludo1():
+    print("Bienvenidos a funciones en Python")
+
+saludo1()
+
+def saludo2(nombre):
+    print("Hola "+nombre+" bienvenido a funciones en Python")
+
+saludo2("Juan")
+saludo2("Luis")
+saludo2("Carlos")
---
+++ Idat_Python/Ejemplos_02.py
@@ -0,0 +1,11 @@
+def saludo3():
+    return "En la semana 12 el tema es funciones"
+
+texto = saludo3()
+print(texto)
+
+def saludo4(nombre):
+    return "Hola "+nombre+" hoy aprenderás funciones"
+
+texto=saludo4("Luisa")
+print(texto)
